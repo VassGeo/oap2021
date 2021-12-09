ff4399871f0368f57f58db4da0c633c3f9b58a91
VassGeo <95747722+VassGeo@users.noreply.github.com>
2021-12-09 08:32:03 +0200

    Update Measurements.py

--- Measurements.py
+++ Measurements.py
@@ -1,19 +1,19 @@
-
-
-# Instructions https://adventofcode.com/2021/day/1
-
-'''def increase_measurements():
-    depth=[199,200,208,210,200,207,240,269,260,263]
-    a=0
-    print(len(depth))
-    for x in range(len(depth)-1):
-        if(depth[x+1]>depth[x]):
-            a=a+1
-    print(a)'''
-
-depth=[199,200,208,210,200,207,240,269,260,263,262]
-def increase_measurements():
- return sum([1 for x in range(len(depth)-1) if(depth[x+1]>depth[x])])
-    
+depth=[]
+a1=0
+with open('input', 'r') as f:
+    depth=f.read().split()
+    for index, x in enumerate (depth):
+        if(index==0):
+            prev_el =int(depth[index])
+        else:
+            if (int(prev_el)<int(depth[index])):
+                
+                prev_el=int(depth[index])
+                a1+=1
+                
+            else:
+                prev_el=int(depth[index])
+                
             
-print(increase_measurements())
+
+print(a1)
